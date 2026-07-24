7eaae90dda3de17a47f5dc1671ddc75f476fe16b
Dhanush Arumugam <your_github_email@example.com>
2026-07-24 15:44:14 +0530

    Your commit message

--- Studentproject/settings.py
+++ Studentproject/settings.py
@@ -157,15 +157,16 @@ LOGOUT_REDIRECT_URL = "login"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
+
+EMAIL_HOST_USER = 'akash@inessconsulting.com'
+
+EMAIL_HOST_PASSWORD = 'xpyagkdoljfygqrb'
+
 EMAIL_HOST = config("EMAIL_HOST", default="smtp.gmail.com")
 
 EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
 
 EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
-
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
